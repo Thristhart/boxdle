a087b58824c59e59e71af3ac26e79be1bd8be1da
Tom Shea <thshea@microsoft.com>
2022-04-09 06:50:14 -0700

    don't treat lit as external

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    build: {
-        rollupOptions: {
-            external: /^lit/,
-        },
-    },
-});
+export default defineConfig({});
 
